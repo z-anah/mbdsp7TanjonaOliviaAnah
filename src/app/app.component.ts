88ac80cd849bd3eae19114c4e344ae38dc7d46c9
RAZAIHARINOSY <oliviah1razaiharinosy@gmail.com>
2021-06-13 18:31:21 +0300

    front page d'acceuil à peut près la dernier version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenusComponent } from './menus/menus.component';
+import { PopularPostComponent } from './popular-post/popular-post.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenusComponent,
+    PopularPostComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
