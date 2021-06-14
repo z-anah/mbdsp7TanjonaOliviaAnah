c507519759f521cd3a6d67cd1410c8bddf6578a1
Randrianomentsoa Tanjona <rtanjonagael@gmail.com>
2021-06-14 20:40:49 +0300

    upload file

---
+++ middleware/upload.js
@@ -0,0 +1,22 @@
+const util = require("util");
+const cors = require("cors");
+const multer = require("multer");
+const maxSize = 2 * 1024 * 1024;
+
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "assets/img");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file);
+  }
+});
+let uploadFile = multer({
+    storage: storage,
+  //limits: { fileSize: maxSize }
+}).single("profil");
+
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-let express = require("express");
+/*let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
 
-let assignment = require("./routes/assignments");
+/*let assignment = require("./routes/assignments");
 let users = require("./routes/users");
 let role = require("./routes/roles");
 let matieres = require("./routes/matieres");
@@ -24,17 +24,17 @@ const options = {
 
 mongoose.connect(uri, options).then(
   () => {
-    console.log("Connecté à la base MongoDB assignments dans le cloud !");
+    console.log("Connecté à la base MongoDB pari dans le cloud !");
     console.log("at URI = " + uri);
     console.log(
-      "vérifiez with http://localhost:8010/api/assignments que cela fonctionne"
+      "vérifiez with http://localhost:8010/api/pari que cela fonctionne"
     );
   },
   (err) => {
     console.log("Erreur de connexion: ", err);
   }
 );
-
+const controller = require("./controller/file.controller");
 // Pour accepter les connexions cross-domain (CORS)
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -55,13 +55,34 @@ let port = process.env.PORT || 8010;
 // les routes
 const prefix = "/api";
 
-app.route(prefix + "/assignments").get(assignment.getAssignments);
+//app.route(prefix + "/assignments").get(assignment.getAssignments);
 
-
+app.route(prefix + "/upload").post(controller.upload);
 // route pour matieres
-app.route(prefix + "/matieres").get(matieres.getMatieres);
+//app.route(prefix + "/matieres").get(matieres.getMatieres);
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
 console.log("Serveur démarré sur http://localhost:" + port);
 
-module.exports = app;
+module.exports = app;*/
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+global.__basedir = __dirname;
+
+var corsOptions = {
+  origin: "http://localhost:8010"
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./routes/route");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let port = 8010;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
