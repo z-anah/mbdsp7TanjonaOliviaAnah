642d89494f7c73063d801076af5799b4ec155194
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2021-06-30 06:17:08 +0300

    Update Match.cs
    init

--- src/dao/view/user-control/panel/Match.cs
+++ src/dao/view/user-control/panel/Match.cs
@@ -28,6 +28,156 @@ namespace pari.src.dao.view.user_control.panel
             InitializeComponent();
         }
 
+
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pariTitle1 = new pari.src.dao.view.user_control.PariTitle();
+            this.pariTextBox1 = new pari.src.dao.view.user_control.PariTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pariComboItem1 = new pari.src.dao.view.user_control.PariComboItem();
+            this.pariComboItem2 = new pari.src.dao.view.user_control.PariComboItem();
+            this.pariComboItem3 = new pari.src.dao.view.user_control.PariComboItem();
+            this.pariComboItem4 = new pari.src.dao.view.user_control.PariComboItem();
+            this.pariComboItem5 = new pari.src.dao.view.user_control.PariComboItem();
+            this.pariComboItem6 = new pari.src.dao.view.user_control.PariComboItem();
+            this.pariDate1 = new pari.src.dao.view.user_control.PariDate();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pariTitle1);
+            this.flowLayoutPanel1.Controls.Add(this.pariTextBox1);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.pariComboItem1);
+            this.flowLayoutPanel1.Controls.Add(this.pariComboItem2);
+            this.flowLayoutPanel1.Controls.Add(this.pariComboItem3);
+            this.flowLayoutPanel1.Controls.Add(this.pariComboItem4);
+            this.flowLayoutPanel1.Controls.Add(this.pariComboItem6);
+            this.flowLayoutPanel1.Controls.Add(this.pariComboItem5);
+            this.flowLayoutPanel1.Controls.Add(this.pariDate1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(634, 513);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pariTitle1
+            // 
+            this.pariTitle1.Location = new System.Drawing.Point(3, 3);
+            this.pariTitle1.Name = "pariTitle1";
+            this.pariTitle1.Size = new System.Drawing.Size(200, 23);
+            this.pariTitle1.TabIndex = 0;
+            // 
+            // pariTextBox1
+            // 
+            this.pariTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pariTextBox1.Location = new System.Drawing.Point(209, 3);
+            this.pariTextBox1.Name = "pariTextBox1";
+            this.pariTextBox1.Size = new System.Drawing.Size(276, 74);
+            this.pariTextBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pariComboItem1
+            // 
+            this.pariComboItem1.Location = new System.Drawing.Point(3, 83);
+            this.pariComboItem1.Name = "pariComboItem1";
+            this.pariComboItem1.Size = new System.Drawing.Size(300, 70);
+            this.pariComboItem1.TabIndex = 3;
+            // 
+            // pariComboItem2
+            // 
+            this.pariComboItem2.Location = new System.Drawing.Point(309, 83);
+            this.pariComboItem2.Name = "pariComboItem2";
+            this.pariComboItem2.Size = new System.Drawing.Size(300, 70);
+            this.pariComboItem2.TabIndex = 4;
+            // 
+            // pariComboItem3
+            // 
+            this.pariComboItem3.Location = new System.Drawing.Point(3, 159);
+            this.pariComboItem3.Name = "pariComboItem3";
+            this.pariComboItem3.Size = new System.Drawing.Size(300, 70);
+            this.pariComboItem3.TabIndex = 5;
+            // 
+            // pariComboItem4
+            // 
+            this.pariComboItem4.Location = new System.Drawing.Point(309, 159);
+            this.pariComboItem4.Name = "pariComboItem4";
+            this.pariComboItem4.Size = new System.Drawing.Size(300, 70);
+            this.pariComboItem4.TabIndex = 6;
+            // 
+            // pariComboItem5
+            // 
+            this.pariComboItem5.Location = new System.Drawing.Point(309, 235);
+            this.pariComboItem5.Name = "pariComboItem5";
+            this.pariComboItem5.Size = new System.Drawing.Size(300, 70);
+            this.pariComboItem5.TabIndex = 7;
+            // 
+            // pariComboItem6
+            // 
+            this.pariComboItem6.Location = new System.Drawing.Point(3, 235);
+            this.pariComboItem6.Name = "pariComboItem6";
+            this.pariComboItem6.Size = new System.Drawing.Size(300, 70);
+            this.pariComboItem6.TabIndex = 8;
+            // 
+            // pariDate1
+            // 
+            this.pariDate1.Location = new System.Drawing.Point(3, 311);
+            this.pariDate1.Name = "pariDate1";
+            this.pariDate1.Size = new System.Drawing.Size(554, 113);
+            this.pariDate1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Match
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Match";
+            this.Size = new System.Drawing.Size(634, 513);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+            pariTitle1.Label.Text = "Ajouter un match";
+            pariTextBox1.Label.Text = "Importer un fichier";
+            pariTextBox1.TextBox.PlaceholderText = "*.json; *.csv; *.excel";
+            pariTextBox1.LabelError.Text = "";
+            button1.Text = "Importer";
+            compet.PariTitle.Label.Text = "Événements";
+            compet.ComboBox.Text = "Événements";
+            eq1.PariTitle.Label.Text = "Équipe 1";
+            eq1.ComboBox.Text = "Équipe 1";
+            eq2.PariTitle.Label.Text = "Équipe 2";
+            eq2.ComboBox.Text = "Équipe 2";
+            formationEq1.PariTitle.Label.Text = "Formation équipe 1";
+            formationEq1.ComboBox.Text = "Formations";
+            formationEq2.PariTitle.Label.Text = "Formation équipe 2";
+            formationEq2.ComboBox.Text = "Formations";
+            arbitre.PariTitle.Label.Text = "Arbitre";
+            arbitre.ComboBox.Text = "Arbitres";
+            pariDate1.PariTitle.Label.Text = "Date et heure";
+            ajouter.Text = "Ajouter";
+        }
+
         public PariTitle PariTitle { get => pariTitle1; set { pariTitle1 = value; pariTitle = value; } }
         public PariTextBox PariTextBox { get => pariTextBox1; set { pariTextBox1 = value; pariTextBox = value; } }
         public Button Importer { get => button1; set { button1 = value; importer = value; } }
