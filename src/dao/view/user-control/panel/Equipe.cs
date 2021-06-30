a2895d95c61b8da0ef85b5624d0bf6302c16024e
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2021-06-30 05:46:20 +0300

    Update Equipe.cs
    Init

--- src/dao/view/user-control/panel/Equipe.cs
+++ src/dao/view/user-control/panel/Equipe.cs
@@ -25,6 +25,140 @@ namespace pari.src.dao.view.user_control.panel
         public Equipe()
         {
             InitializeComponent();
+        }
+
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Equipe));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pariTitle1 = new pari.src.dao.view.user_control.PariTitle();
+            this.pariTextBox1 = new pari.src.dao.view.user_control.PariTextBox();
+            this.pariTextBox2 = new pari.src.dao.view.user_control.PariTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pariTextBox3 = new pari.src.dao.view.user_control.PariTextBox();
+            this.pariTextBox4 = new pari.src.dao.view.user_control.PariTextBox();
+            this.pariTitle2 = new pari.src.dao.view.user_control.PariTitle();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pariLabelError1 = new pari.src.dao.view.user_control.PariLabelError();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pariTitle1);
+            this.flowLayoutPanel1.Controls.Add(this.pariTextBox1);
+            this.flowLayoutPanel1.Controls.Add(this.pariTextBox2);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.pariTextBox3);
+            this.flowLayoutPanel1.Controls.Add(this.pariTextBox4);
+            this.flowLayoutPanel1.Controls.Add(this.pariTitle2);
+            this.flowLayoutPanel1.Controls.Add(this.richTextBox1);
+            this.flowLayoutPanel1.Controls.Add(this.pariLabelError1);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(634, 513);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pariTitle1
+            // 
+            this.pariTitle1.Location = new System.Drawing.Point(3, 3);
+            this.pariTitle1.Name = "pariTitle1";
+            this.pariTitle1.Size = new System.Drawing.Size(424, 23);
+            this.pariTitle1.TabIndex = 0;
+            // 
+            // pariTextBox1
+            // 
+            this.pariTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pariTextBox1.Location = new System.Drawing.Point(3, 32);
+            this.pariTextBox1.Name = "pariTextBox1";
+            this.pariTextBox1.Size = new System.Drawing.Size(262, 74);
+            this.pariTextBox1.TabIndex = 1;
+            // 
+            // pariTextBox2
+            // 
+            this.pariTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.pariTextBox2.Location = new System.Drawing.Point(271, 32);
+            this.pariTextBox2.Name = "pariTextBox2";
+            this.pariTextBox2.Size = new System.Drawing.Size(263, 74);
+            this.pariTextBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Importer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pariTextBox3
+            // 
+            this.pariTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.pariTextBox3.Location = new System.Drawing.Point(3, 112);
+            this.pariTextBox3.Name = "pariTextBox3";
+            this.pariTextBox3.Size = new System.Drawing.Size(262, 74);
+            this.pariTextBox3.TabIndex = 3;
+            // 
+            // pariTextBox4
+            // 
+            this.pariTextBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.pariTextBox4.Location = new System.Drawing.Point(271, 112);
+            this.pariTextBox4.Name = "pariTextBox4";
+            this.pariTextBox4.Size = new System.Drawing.Size(263, 74);
+            this.pariTextBox4.TabIndex = 8;
+            // 
+            // pariTitle2
+            // 
+            this.pariTitle2.Location = new System.Drawing.Point(3, 192);
+            this.pariTitle2.Name = "pariTitle2";
+            this.pariTitle2.Size = new System.Drawing.Size(407, 23);
+            this.pariTitle2.TabIndex = 4;
+            this.pariTitle2.Load += new System.EventHandler(this.pariTitle2_Load);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 221);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(628, 109);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // pariLabelError1
+            // 
+            this.pariLabelError1.Location = new System.Drawing.Point(3, 336);
+            this.pariLabelError1.Name = "pariLabelError1";
+            this.pariLabelError1.Size = new System.Drawing.Size(567, 25);
+            this.pariLabelError1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Equipe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Equipe";
+            this.Size = new System.Drawing.Size(634, 513);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
             pariTitle1.Label.Text = "Ajouter équipe";
             pariTextBox1.Label.Text = "Nom de l'équipe";
             pariTextBox1.TextBox.PlaceholderText = "Nom de l'équipe";
@@ -56,6 +190,19 @@ namespace pari.src.dao.view.user_control.panel
         public Button Importer { get => button2; set { button2 = value; importer = value; } }
 
         private void pariTitle2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;...";
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK) pariTextBox2.TextBox.Text = dialog.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
