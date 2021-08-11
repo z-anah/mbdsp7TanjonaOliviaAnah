eb71240b690bcaaa9239a8a90e82ba9f5538ae67
RAZAIHARINOSY <oliviah1razaiharinosy@gmail.com>
2021-08-11 09:37:05 +0300

    Get equipe by Id

--- service/equipe.js
+++ service/equipe.js
@@ -1,6 +1,6 @@
 const { aggregate } = require("../model/equipes");
 let Equipe = require("../model/equipes");
-
+const mongoose = require("mongoose");
 // Récupérer tous les equipe (GET)
 function getListEquipe(req, res) {
   let aggregate = Equipe.aggregate([
@@ -27,4 +27,27 @@ function getListEquipe(req, res) {
   });
 }
 
-module.exports = { getListEquipe };
+function getEquipeById(req, res) {
+  let id = parseInt(req.params.idequipe);
+  let aggregate = Equipe.aggregate([
+    { $match: { _id: { $in: [mongoose.Types.ObjectId(req.params.idequipe)] } } },
+    {
+      $lookup: {
+        from: "formation",
+        localField: "idformation",
+        foreignField: "id",
+        as: "formation",
+      },
+    },
+  ]);
+
+  let options = {};
+  // callback
+  Equipe.aggregatePaginate(aggregate, options, (err, equipe) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(equipe);
+  });
+}
+module.exports = { getListEquipe, getEquipeById };
